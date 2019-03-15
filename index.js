d990060f862ce8f873f70b4e146056b6306cf4d3
Anthony Zotti <Anthonymzotti@gmail.com>
2019-03-14 20:55:30 -0400

    Create MUD client

---
+++ index.js
@@ -0,0 +1,53 @@
+// get references to the required stuff
+var TelnetSocket, net, socket, tSocket;
+
+const { username, password } = require("./secrets.js");
+
+net = require("net");
+
+({TelnetSocket} = require("telnet-stream"));
+
+// create a Socket connection
+socket = net.createConnection(8000, "mud.accursed-lands.com");
+
+// decorate the Socket connection as a TelnetSocket
+tSocket = new TelnetSocket(socket);
+
+// if the socket closes, terminate the program
+tSocket.on("close", function() {
+  return process.exit();
+});
+
+// Output from mud
+tSocket.on("data", function(buffer) {
+	const output = buffer.toString("utf8");
+
+	runTriggers(output);
+
+  return process.stdout.write(output);
+});
+
+
+// Input to MUD
+process.stdin.on("data", function(buffer) {
+	const input = buffer.toString("utf8");	
+  return tSocket.write(input);
+});
+
+const triggers = [
+	{ trigger: /Please enter an account name:/g, input: username},
+	{ trigger: /What is your password?/g, input: password },
+];
+
+function runTriggers(text) {
+	triggers.forEach(({trigger, input}) => {
+		if (text.match(trigger)) {
+			send(input);
+		}	
+	});
+}
+	
+
+function send(text) {
+	tSocket.write(`${text}\n`);
+}
